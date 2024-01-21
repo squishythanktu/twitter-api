1cf199ca4725768c51594409d7c5d7a0680b82bf
Le Thanh Tu (YooMie) <87054146+squishythanktu@users.noreply.github.com>
2024-01-21 21:42:09 +0700

    feat: user schema

---
+++ src/controllers/users.controllers.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from 'express'
+import usersService from '~/services/users.services'
+
+export const loginController = (req: Request, res: Response) => {
+  res.json({ message: 'Login successfully' })
+}
+
+export const registerController = async (req: Request, res: Response) => {
+  const { email, password } = req.body
+
+  try {
+    const result = await usersService.register({ email, password })
+    return res.json({ message: 'Register successfully', result })
+  } catch (error) {
+    return res.status(400).json({
+      message: 'Register failed',
+      error
+    })
+  }
+}
