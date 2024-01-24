9db83b31fcec1b41adeb825fda436574ace4c302
Le Thanh Tu (YooMie) <87054146+squishythanktu@users.noreply.github.com>
2024-01-24 23:29:01 +0700

    feat: register validator

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,3 +1,4 @@
+import { registerValidator } from './../middlewares/users.middlewares'
 import { Router } from 'express'
 import { loginController, registerController } from '~/controllers/users.controllers'
 import { loginValidator } from '~/middlewares/users.middlewares'
@@ -5,6 +6,6 @@ import { loginValidator } from '~/middlewares/users.middlewares'
 const usersRouter = Router()
 
 usersRouter.post('/login', loginValidator, loginController)
-usersRouter.post('/register', registerController)
+usersRouter.post('/register', registerValidator, registerController)
 
 export default usersRouter
