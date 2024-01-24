b8cc955a56e2661aedc68ff74c9dc09e90d3f84c
Le Thanh Tu (YooMie) <87054146+squishythanktu@users.noreply.github.com>
2024-01-24 23:29:01 +0700

    feat: register validator

--- src/services/users.services.ts
+++ src/services/users.services.ts
@@ -13,6 +13,11 @@ class UsersService {
 
     return result
   }
+
+  async checkEmailExist(email: string) {
+    const user = await databaseService.users.findOne({ email })
+    return !!user
+  }
 }
 
 const usersService = new UsersService()
