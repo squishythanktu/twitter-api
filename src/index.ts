99e55aa7cb39577ee1e69dae554ec7bf42a04611
Le Thanh Tu (YooMie) <87054146+squishythanktu@users.noreply.github.com>
2024-01-29 22:57:02 +0700

    feat: standardize error handlers

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
 import express from 'express'
 import usersRouter from './routes/users.routes'
 import databaseService from './services/database.services'
+import { defaultErrorHandler } from './middlewares/error.middlewares'
 
+databaseService.connect()
 const app = express()
 const port = 3000
 
 app.use(express.json())
 app.use('/users', usersRouter)
 
-databaseService.connect()
+// Default error handler
+app.use(defaultErrorHandler)
 
 app.listen(port, () => {
   console.log(`App running on port ${port}`)
--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -1,6 +1,8 @@
 import express from 'express'
 import { validationResult, ValidationChain } from 'express-validator'
 import { RunnableValidationChains } from 'express-validator/src/middlewares/schema'
+import HTTP_STATUS from '~/constants/httpStatus'
+import { EntityError, ErrorWithStatus } from '~/models/Errors'
 // can be reused by many routes
 
 // sequential processing, stops running validations chain if the previous one fails.
@@ -13,6 +15,16 @@ export const validate = (validation: RunnableValidationChains<ValidationChain>) 
       return next()
     }
 
-    res.status(400).json({ errors: errors.mapped() })
+    const errorsObject = errors.mapped()
+    const entityError = new EntityError({ errors: {} })
+
+    for (const key in errorsObject) {
+      const { msg } = errorsObject[key]
+      // Return the error that is not validation error
+      if (msg instanceof ErrorWithStatus && msg.status !== HTTP_STATUS.UNPROCESSABLE_ENTITY) return next(msg)
+      entityError.errors[key] = errorsObject[key]
+    }
+
+    next(entityError)
   }
 }
