778b906fb1be7298b24c9d313493f26f7ded8109
Le Thanh Tu (YooMie) <87054146+squishythanktu@users.noreply.github.com>
2024-01-29 22:57:02 +0700

    feat: standardize error handlers

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from 'express'
+import { NextFunction, Request, Response } from 'express'
 import usersService from '~/services/users.services'
 import { ParamsDictionary } from 'express-serve-static-core'
 import { RegisterReqBody } from '~/models/requests/User.request'
@@ -7,14 +7,11 @@ export const loginController = (req: Request, res: Response) => {
   res.json({ message: 'Login successfully' })
 }
 
-export const registerController = async (req: Request<ParamsDictionary, any, RegisterReqBody>, res: Response) => {
-  try {
-    const result = await usersService.register(req.body)
-    return res.json({ message: 'Register successfully', result })
-  } catch (error) {
-    return res.status(400).json({
-      message: 'Register failed',
-      error
-    })
-  }
+export const registerController = async (
+  req: Request<ParamsDictionary, any, RegisterReqBody>,
+  res: Response,
+  next: NextFunction
+) => {
+  const result = await usersService.register(req.body)
+  return res.json({ message: 'Register successfully', result })
 }
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -2,10 +2,11 @@ import { registerValidator } from './../middlewares/users.middlewares'
 import { Router } from 'express'
 import { loginController, registerController } from '~/controllers/users.controllers'
 import { loginValidator } from '~/middlewares/users.middlewares'
+import { wrapRequestHandler } from '~/utils/handlers'
 
 const usersRouter = Router()
 
 usersRouter.post('/login', loginValidator, loginController)
-usersRouter.post('/register', registerValidator, registerController)
+usersRouter.post('/register', registerValidator, wrapRequestHandler(registerController))
 
 export default usersRouter
