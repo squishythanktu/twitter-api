4efdfd5b4a845741e45afae379f716c8c9a4523f
Le Thanh Tu (YooMie) <87054146+squishythanktu@users.noreply.github.com>
2024-02-25 18:51:59 +0700

    feat: logout

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from 'express'
 import usersService from '~/services/users.services'
 import { ParamsDictionary } from 'express-serve-static-core'
-import { RegisterReqBody } from '~/models/requests/User.request'
+import { LogoutReqBody, RegisterReqBody } from '~/models/requests/User.request'
 import { ObjectId } from 'mongodb'
 import User from '~/models/schemas/User.schema'
 import { USERS_MESSAGES } from '~/constants/messages'
@@ -23,4 +23,10 @@ export const registerController = async (
 ) => {
   const result = await usersService.register(req.body)
   return res.json({ message: USERS_MESSAGES.REGISTER_SUCCESS, result })
+}
+
+export const logoutController = async (req: Request<ParamsDictionary, any, LogoutReqBody>, res: Response) => {
+  const { refresh_token } = req.body
+  const result = await usersService.logout(refresh_token)
+  return res.json(result)
 }
