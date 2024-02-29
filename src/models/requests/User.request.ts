362e634e878f6d9a715c7daeb52d2e03a7e6b8c3
Le Thanh Tu (YooMie) <87054146+squishythanktu@users.noreply.github.com>
2024-02-29 11:42:14 +0700

    feat: resend verify email

--- src/models/requests/User.request.ts
+++ src/models/requests/User.request.ts
@@ -1,6 +1,11 @@
 import { JwtPayload } from 'jsonwebtoken'
 import { TokenType } from '~/constants/enums'
 
+export interface LoginReqBody {
+  email: string
+  password: string
+}
+
 export interface RegisterReqBody {
   name: string
   email: string
@@ -16,4 +21,8 @@ export interface LogoutReqBody {
 export interface TokenPayload extends JwtPayload {
   user_id: string
   token_type: TokenType
+}
+
+export interface VerifyEmailReqBody {
+  email_verify_token: string
 }
