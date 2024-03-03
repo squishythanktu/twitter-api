d7726b8e5f2f7f9ea463a6ef6d60182c740107e4
Le Thanh Tu (YooMie) <87054146+squishythanktu@users.noreply.github.com>
2024-03-03 11:00:02 +0700

    feat: reset password

--- src/services/users.services.ts
+++ src/services/users.services.ts
@@ -172,6 +172,22 @@ class UsersService {
       message: USERS_MESSAGES.CHECK_EMAIL_TO_RESET_PASSWORD
     }
   }
+
+  async resetPassword(userr_id: string, password: string) {
+    await databaseService.users.updateOne({ _id: new ObjectId(userr_id) }, [
+      {
+        $set: {
+          forgot_password_token: '',
+          password: hashPassword(password),
+          updated_at: '$$NOW'
+        }
+      }
+    ])
+
+    return {
+      message: USERS_MESSAGES.RESET_PASSWORD_SUCCESS
+    }
+  }
 }
 
 const usersService = new UsersService()
