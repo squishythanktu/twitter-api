ad097f153965624d47ceaaf056da74a0646949c4
Thanh Tu (YooMie) <87054146+squishythanktu@users.noreply.github.com>
2024-04-29 22:55:23 +0700

    feat: change password (#7)

--- src/services/users.services.ts
+++ src/services/users.services.ts
@@ -303,6 +303,22 @@ class UsersService {
     })
     return { message: USERS_MESSAGES.UNFOLLOW_SUCCESS }
   }
+
+  async changePassword(user_id: string, password: string) {
+    await databaseService.users.updateOne(
+      { _id: new ObjectId(user_id) },
+      {
+        $set: {
+          password: hashPassword(password)
+        },
+        $currentDate: {
+          updated_at: true
+        }
+      }
+    )
+
+    return { message: USERS_MESSAGES.CHANGE_PASSWORD_SUCCESS }
+  }
 }
 
 const usersService = new UsersService()
