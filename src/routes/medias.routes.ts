c54ae1d32df5ccbee8dc41848555b3d9dae993bf
Le Thanh Tu (YooMie) <87054146+squishythanktu@users.noreply.github.com>
2024-07-01 20:28:24 +0700

    feat: upload multi images

--- src/routes/medias.routes.ts
+++ src/routes/medias.routes.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express'
 import formidable from 'formidable'
 import path from 'path'
-import { uploadSingleImageController } from '~/controllers/medias.controllers'
+import { uploadImagesController } from '~/controllers/medias.controllers'
 import { wrapRequestHandler } from '~/utils/handlers'
 
 const mediasRouter = Router()
 
-mediasRouter.post('/upload-image', wrapRequestHandler(uploadSingleImageController))
+mediasRouter.post('/upload-image', wrapRequestHandler(uploadImagesController))
 
 export default mediasRouter
--- src/utils/file.ts
+++ src/utils/file.ts
@@ -1,24 +1,25 @@
 import { Request } from 'express'
-import formidable, { File } from 'formidable'
+import { File } from 'formidable'
 import fs from 'fs'
 import { UPLOAD_TEMP_DIR } from '~/constants/dir'
 
 export const initFolder = () => {
-  const uploadFolderPath = UPLOAD_TEMP_DIR
-  if (!fs.existsSync(uploadFolderPath)) {
-    fs.mkdirSync(uploadFolderPath, {
-      recursive: true // mục đích để tạo folder nested
+  if (!fs.existsSync(UPLOAD_TEMP_DIR)) {
+    fs.mkdirSync(UPLOAD_TEMP_DIR, {
+      recursive: true // mục đích là để tạo folder nested
     })
   }
 }
 
-export const handleUploadSingleImage = async (req: Request) => {
+export const handleUploadImages = async (req: Request) => {
+  const formidable = (await import('formidable')).default
   const form = formidable({
     uploadDir: UPLOAD_TEMP_DIR,
-    maxFiles: 1,
+    maxFiles: 4,
     keepExtensions: true,
-    maxFileSize: 300 * 1024, //300KB,
-    filter: ({ name, originalFilename, mimetype }) => {
+    maxFileSize: 300 * 1024, // 300KB
+    maxTotalFileSize: 300 * 1024 * 4,
+    filter: function ({ name, originalFilename, mimetype }) {
       const valid = name === 'image' && Boolean(mimetype?.includes('image/'))
       if (!valid) {
         form.emit('error' as any, new Error('File type is not valid') as any)
@@ -26,22 +27,21 @@ export const handleUploadSingleImage = async (req: Request) => {
       return valid
     }
   })
-
-  return new Promise<File>((resolve, reject) => {
+  return new Promise<File[]>((resolve, reject) => {
     form.parse(req, (err, fields, files) => {
       if (err) {
-        reject(err)
+        return reject(err)
       }
-      if (!files.image) {
+      // eslint-disable-next-line no-extra-boolean-cast
+      if (!Boolean(files.image)) {
         return reject(new Error('File is empty'))
       }
-
-      resolve((files.image as File[])[0])
+      resolve(files.image as File[])
     })
   })
 }
 
-export const getNameFromFullName = (fullname: string) => {
+export const getNameFromFullname = (fullname: string) => {
   const namearr = fullname.split('.')
   namearr.pop()
   return namearr.join('')
